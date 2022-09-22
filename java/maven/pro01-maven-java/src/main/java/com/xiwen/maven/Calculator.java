81a37801bd76fda2cb25a6792cd41bfa2c7d3354
yf <15133695630@163.com>
2022-09-22 16:05:20 +0800

    解析pom标签的作用

---
+++ java/maven/pro01-maven-java/src/main/java/com/xiwen/maven/Calculator.java
@@ -0,0 +1,9 @@
+package com.xiwen.maven;
+
+public class Calculator {
+
+    public int sum(int i, int j){
+        return i + j;
+    }
+
+}
---
+++ java/maven/pro01-maven-java/src/test/java/com/xiwen/maven/CalculatorTest.java
@@ -0,0 +1,28 @@
+package com.xiwen.maven;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void testSum(){
+
+        // 1.创建Calculator对象
+        Calculator calculator = new Calculator();
+
+        // 2.调用Calculator对象的方法，获取到程序运行实际的结果
+        int actualResult = calculator.sum(5, 3);
+
+        // 3.声明一个变量，表示程序运行期待的结果
+        int expectedResult = 8;
+
+        // 4.使用断言来判断实际结果和期待结果是否一致
+        // 如果一致：测试通过，不会抛出异常
+        // 如果不一致：抛出异常，测试失败
+        assertEquals(expectedResult, actualResult);
+        System.out.println("测试类是否成功");
+
+    }
+}
