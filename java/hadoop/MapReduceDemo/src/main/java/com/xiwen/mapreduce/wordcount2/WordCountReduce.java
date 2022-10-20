00ae9a316f24318a8d452a6ceda649d0f4605a2b
yf <15133695630@163.com>
2022-10-20 10:47:05 +0800

    配置pom文件中的打包插件在分布式hadoop中运行测试

---
+++ java/hadoop/MapReduceDemo/src/main/java/com/xiwen/mapreduce/wordcount2/WordCountReduce.java
@@ -0,0 +1,21 @@
+package com.xiwen.mapreduce.wordcount2;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class WordCountReduce extends Reducer<Text, IntWritable,Text,IntWritable> {
+
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Reducer<Text, IntWritable, Text, IntWritable>.Context context) throws IOException, InterruptedException {
+
+        int sum = 0;
+        for (IntWritable value : values) {
+            sum += value.get();
+        }
+        context.write(key, new IntWritable(sum));
+
+    }
+}
