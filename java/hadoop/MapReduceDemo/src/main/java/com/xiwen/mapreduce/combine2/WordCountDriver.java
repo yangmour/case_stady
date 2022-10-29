e67a56360fe36b3e840cb9f4a96901cc12f3edba
yf <15133695630@163.com>
2022-10-29 16:29:15 +0800

    combiner学习，在map阶段就进行合并，在reduce阶段之前。在不影响最终结果的情况下可以使用combiner类进行提前合并，减轻reduce阶段的数据量过大数据倾斜问题。

---
+++ java/hadoop/MapReduceDemo/src/main/java/com/xiwen/mapreduce/combine2/WordCountDriver.java
@@ -0,0 +1,53 @@
+package com.xiwen.mapreduce.combine2;
+
+import com.xiwen.mapreduce.combine.WordCountCombiner;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class WordCountDriver {
+
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+
+        //7步
+
+        //1.获取配置信息以及获取job对象
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf);
+        //2.关联Driver程序的jar
+        job.setJarByClass(WordCountDriver.class);
+        //3.关联mapper和 reduce的jar
+        job.setMapperClass(WordCountMapper.class);
+
+        //代码逻辑和reduce的逻辑一样就直接可以使用这个类进行
+        job.setCombinerClass(WordCountReduce.class);
+
+        job.setReducerClass(WordCountReduce.class);
+
+        //4.设置mapper的输出类型
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        //5.设置最终的输出类型
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        // 6.设置输入输出路径
+        // FileInputFormat.setInputPaths(job, new Path("D:\\software\\IDE\\Git\\prodect\\case_study\\java\\hadoop\\MapReduceDemo\\input\\hello"));
+        FileInputFormat.setInputPaths(job, new Path("input/combine"));
+        FileOutputFormat.setOutputPath(job, new Path("output"));
+        //7.提交job任务
+
+        boolean result = job.waitForCompletion(true);
+
+        System.exit(result ? 0 : 1);
+
+
+    }
+}
