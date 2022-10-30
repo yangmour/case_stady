de72c388664be71962dad5398a0eca537ef2c3b7
yf <15133695630@163.com>
2022-10-30 16:19:49 +0800

    驱动中修改队列为hive

---
+++ java/hadoop/MapReduceDemo/src/main/java/com/xiwen/mapreduce/scheduler/WordCountDriver.java
@@ -0,0 +1,51 @@
+package com.xiwen.mapreduce.scheduler;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class WordCountDriver {
+
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+
+        //7步
+
+        //1.获取配置信息以及获取job对象
+        Configuration conf = new Configuration();
+
+        conf.set("mapreduce.job.queuename","hive");
+
+        Job job = Job.getInstance(conf);
+        //2.关联Driver程序的jar
+        job.setJarByClass(WordCountDriver.class);
+        //3.关联mapper和 reduce的jar
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountReduce.class);
+
+        //4.设置mapper的输出类型
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        //5.设置最终的输出类型
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        //6.设置输入输出路径
+        //FileInputFormat.setInputPaths(job, new Path("D:\\software\\IDE\\Git\\prodect\\case_study\\java\\hadoop\\MapReduceDemo\\input\\hello"));
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        //7.提交job任务
+
+        boolean result = job.waitForCompletion(true);
+
+        System.exit(result ? 0 : 1);
+
+
+    }
+}
