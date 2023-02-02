f8e52588d4ace3dbe81994186dee8e88fb8ffe7c
yf <15133695630@163.com>
2023-02-02 12:00:43 +0800

    添加public

--- sggcode/20230201Java/code/day0202_01_code/HelloWorld.java
+++ sggcode/20230201Java/code/day0202_01_code/HelloWorld.java
@@ -1,5 +1,13 @@
-class HelloWorld{
+
+/*
+注释:
+开发java程序的3个步骤
+1.编辑java文件，以java为结尾
+2.编译源文件将java的文件编译为.class文件，命令为：javac 文件名.java
+3.在命令行运行class文件，命令：java 类名（注意大小写敏感）
+*/
+public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("浣犲ソ灏氱璋凤紒");
+		System.out.println("你好尚硅谷！");
 	}
 }
