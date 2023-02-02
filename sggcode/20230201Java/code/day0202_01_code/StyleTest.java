8b1cf37b67495b40a242246a509f030a4462d362
yf <15133695630@163.com>
2023-02-02 16:54:34 +0800

    代码格式

---
+++ sggcode/20230201Java/code/day0202_01_code/StyleTest.java
@@ -0,0 +1,26 @@
+/**
+代码格式
+(1) 紧凑型
+class StyleTest{
+	public static void main(String[] args){
+		
+	}
+}
+(2) 宽松型
+class StyleTest
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
+
+缩进：Tab制表位，每一级缩进按一个Tab键，遇到{，表示要缩进。
+运算符的左右两边加空格。
+*/
+
+class StyleTest {
+	public static void main(String[] args) {
+		
+	}
+}
