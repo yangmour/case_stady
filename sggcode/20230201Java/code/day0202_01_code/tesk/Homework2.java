05530d27323153bb1e8ec74e9dc8bcf2d1f84ba0
yf <15133695630@163.com>
2023-02-02 18:55:50 +0800

    自己总结知识点和作业

---
+++ sggcode/20230201Java/code/day0202_01_code/tesk/Homework2.java
@@ -0,0 +1,27 @@
+class Homework2{
+	public static void main(String[] args){
+		byte byteValue = 100;
+		System.out.println("byteValue" +byteValue);
+		short shortValue = 1000;
+		System.out.println("shortValue" + shortValue);
+		int intValue = 123456;
+		System.out.println("intValue" + intValue);
+		long longValue = 12345678900l;
+		System.out.println("longValue" + longValue);
+		float floatValue = 5.5f;
+		System.out.println("floatValue" + floatValue);
+		double doubleValue = 8.5;
+		System.out.println("doubleValue" + doubleValue);
+		char charA = 'a';
+		char char0 = '0';
+		char charS = 'ип';
+		System.out.println( "charA" + charA);
+		System.out.println( "char0" + char0);
+		System.out.println( "charS" + charS);
+		boolean booleanValue = false;
+		System.out.println("booleanValue" + booleanValue);
+		String stringValue = "HelloWorld";
+		System.out.println("stringValue" + stringValue);
+		
+	}
+}
---
+++ sggcode/20230201Java/code/day0202_01_code/tesk/Homework3.java
@@ -0,0 +1,21 @@
+public class Homework3{
+	public static void main(String[] args){
+		String name = "希文";
+		char sex = '男';
+		int age = 24;
+		float height = 183.2f;
+		Double weight = 129.1;
+		boolean  isMarried = false;
+		System.out.println("姓名:" + name);
+		System.out.print("性别:");
+		System.out.println(sex);
+		System.out.println(age);
+		System.out.print("身高:");
+		System.out.println(height);
+		System.out.print("体重:");
+		System.out.println(weight);
+		System.out.print("是否是已婚:");
+		System.out.println(isMarried);
+		
+	}
+}
