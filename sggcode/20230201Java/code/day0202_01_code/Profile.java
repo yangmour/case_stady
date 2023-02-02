c936e493b8311780fb4bc73b585044060b156066
yf <15133695630@163.com>
2023-02-02 15:40:36 +0800

    javadoc文档生成和打印数据类型学习

---
+++ sggcode/20230201Java/code/day0202_01_code/Profile.java
@@ -0,0 +1,29 @@
+
+/**
+学习打印数据类型
+String类型
+char类型
+float单精度类型
+double双精度类型
+boolean类型
+@author xiwen 
+*/
+public class Profile{
+	/**
+	@param args 传入的参数
+	*/
+	public static void main(String[] args){
+		
+		System.out.println("姓名:" + "希文");
+		System.out.print("性别:");
+		System.out.println('男');
+		System.out.println(24);
+		System.out.print("身高:");
+		System.out.println(183.2f);
+		System.out.print("体重:");
+		System.out.println(129.1);
+		System.out.print("是否是本专业:");
+		System.out.println(true);
+		
+	}
+}
