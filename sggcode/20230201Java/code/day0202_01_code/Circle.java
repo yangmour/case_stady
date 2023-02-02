e2dfd84195e639ef5a202d301b503bceb9845d76
yf <15133695630@163.com>
2023-02-02 16:43:25 +0800

    练习final关键字常量值要大写如XXX_YYY_ZZZ

--- sggcode/20230201Java/code/day0202_01_code/Circle.java
+++ sggcode/20230201Java/code/day0202_01_code/Circle.java
@@ -7,13 +7,13 @@ class Circle{
 	public static void main(String[] args){
 		double r1 = 10;
 		double r2 = 20;
-		double pi = 3.14159;
+		final double PI = 3.14159;
 		
-		double s1 = pi * r1 * r1;
-		double c1 =  2 * pi * r1;
+		double s1 = PI * r1 * r1;
+		double c1 =  2 * PI * r1;
 								
-		double s2 = pi * r2 * r2;
-		double c2 =  2 * pi * r2;
+		double s2 = PI * r2 * r2;
+		double c2 =  2 * PI * r2;
 		System.out.println("圆1的面积：" + s1);
 		System.out.println("圆1的周长：" + c1);
 		System.out.println("圆2的面积：" + s2);
