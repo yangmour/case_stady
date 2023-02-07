3f4602140cbdba12ca5bca43580700d0cef3e3bf
yf <15133695630@163.com>
2023-02-07 11:51:57 +0800

    最大公约数最小公倍数添加公式，和嵌套了一层循环一直执行

--- sggcode/20230201Java/code/JavaSE_Code/day0206_code/src/com/xiwen/exercise2/Exercise4.java
+++ sggcode/20230201Java/code/JavaSE_Code/day0206_code/src/com/xiwen/exercise2/Exercise4.java
@@ -11,33 +11,36 @@ import java.util.Scanner;
  */
 public class Exercise4 {
     public static void main(String[] args) {
-
         //4、输入两个正整数m和n，求其最大公约数和最小公倍数
         Scanner scanner = new Scanner(System.in);
-        int m;
-        int n;
-        do {
-            System.out.println("请输入一个正整数:");
-            m = scanner.nextInt();
-            System.out.println("请输入一个正整数:");
-            n = scanner.nextInt();
-        } while (m < 0 || n < 0);
+        while (true) {
+
+            int m;
+            int n;
+            do {
+                System.out.println("请输入一个正整数:");
+                m = scanner.nextInt();
+                System.out.println("请输入一个正整数:");
+                n = scanner.nextInt();
+            } while (m < 0 || n < 0);
 
 
-        int max = m > n ? m : n;
-        int min = m < n ? m : n;
+            int max = m > n ? m : n;
+            int min = m < n ? m : n;
 
-        for (int i = min; i > 0; i--) {
-            if (min % i == 0 && max % i == 0) {
-                System.out.println("最大公约数为:" + i);
-                break;
+            for (int i = min; i > 0; i--) {
+                if (min % i == 0 && max % i == 0) {
+                    System.out.println("最大公约数为:" + i);
+                    System.out.println("最小公倍数为:" + m * n / i);
+                    break;
+                }
             }
-        }
 
-        for (int i = max; i <= m * n; i++) {
-            if (i % max == 0 && i % min == 0) {
-                System.out.println("最小公倍数为:" + i);
-                break;
+            for (int i = max; i <= m * n; i++) {
+                if (i % max == 0 && i % min == 0) {
+                    System.out.println("最小公倍数为:" + i);
+                    break;
+                }
             }
         }
 
