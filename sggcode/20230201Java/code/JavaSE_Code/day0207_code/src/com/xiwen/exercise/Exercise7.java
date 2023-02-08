d0eb93922564dae08230fdcde4f429b2e8b2ece6
yf <15133695630@163.com>
2023-02-08 11:02:54 +0800

    菱形和空心的菱形

--- sggcode/20230201Java/code/JavaSE_Code/day0207_code/src/com/xiwen/exercise/Exercise7.java
+++ sggcode/20230201Java/code/JavaSE_Code/day0207_code/src/com/xiwen/exercise/Exercise7.java
@@ -85,6 +85,96 @@ public class Exercise7 {
             System.out.println();
         }
 
+        System.out.println("-----------------------");
+        /**
+         *    *
+         *   ***
+         *  *****
+         * *******
+         *  *****
+         *   ***
+         *    *
+         */
+        // 上半部分
+        for (int i = 1; i <= 4; i++) {
+            // 空格
+            for (int j = 1; j <= 4 - i; j++) {
+                System.out.print(" ");
+            }
+            // 星星
+            for (int j = 1; j <= i * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            //换行
+            System.out.println();
+        }
+
+        // 下半部分
+        // i=1 j =0  j2=1-5 *****  7-2 * 1 = 5
+        // i=2 j =1  j2=1-3  ***   7-2 * 2 = 3
+        // i=3 j =2  j2=1     *    7-2 * 3 = 1
+
+        for (int i = 1; i < 4; i++) {
+            // 空格
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            // 星星
+            for (int j = 1; j <= 7 - 2 * i; j++) {
+                System.out.print("*");
+            }
+            //换行
+            System.out.println();
+        }
+
+
+        System.out.println("-----------------------");
+        /**
+         *    *
+         *   * *
+         *  *   *
+         * *     *
+         *  *   *
+         *   * *
+         *    *
+         */
+        // 上半部分
+        for (int i = 1; i <= 4; i++) {
+            // 空格
+            for (int j = 1; j <= 4 - i; j++) {
+                System.out.print(" ");
+            }
+            // 星星
+            for (int j = 1; j <= i * 2 - 1; j++) {
+                if (j == 1 || j == i * 2 - 1) {
+                    System.out.print("*");
+                    continue;
+                }
+                System.out.print(" ");
+
+            }
+            //换行
+            System.out.println();
+        }
+
+        // 下半部分
+        // i=1 j =0  j2=1-5 *****  7-2 * 1 = 5
+        // i=2 j =1  j2=1-3  ***   7-2 * 2 = 3
+        // i=3 j =2  j2=1     *    7-2 * 3 = 1
+
+        for (int i = 1; i < 4; i++) {
+            // 空格
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            // 星星
+            for (int j = 1; j <= 7 - 2 * i; j++) {
+                System.out.print(j == 1 || j == 7 - 2 * i ? "*" : " ");
+            }
+            //换行
+            System.out.println();
+        }
+
 
     }
 }
