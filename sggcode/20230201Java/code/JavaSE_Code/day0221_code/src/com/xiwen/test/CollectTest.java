9da574a8bb5aa352670a6d11d4a7ad3f5310473e
yf <15133695630@163.com>
2023-02-21 18:08:05 +0800

    白天Collection的子类ArrayList和iterator迭代器练习题

---
+++ sggcode/20230201Java/code/JavaSE_Code/day0221_code/src/com/xiwen/test/CollectTest.java
@@ -0,0 +1,16 @@
+package com.xiwen.test;
+
+/**
+ * Description:
+ *
+ * @author: yf
+ * @Create: 2023/2/21-8:42
+ * @Version: 1.0
+ */
+public class CollectTest {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder= new StringBuilder("helloworld");
+        stringBuilder.append("adfasfsdafsa").append("aaaaaaaaaaaaaaaaaaaaaaaa");
+
+    }
+}
