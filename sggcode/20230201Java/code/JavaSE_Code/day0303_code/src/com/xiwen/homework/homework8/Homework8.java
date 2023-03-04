8350a65c84f38d6157fd19f79cd3bb750caf857f
yf <15133695630@163.com>
2023-03-04 08:57:02 +0800

    反射和注解练习作业

--- sggcode/20230201Java/code/JavaSE_Code/day0303_code/src/com/xiwen/homework/homework8/Homework8.java
+++ sggcode/20230201Java/code/JavaSE_Code/day0303_code/src/com/xiwen/homework/homework8/Homework8.java
@@ -13,3 +13,35 @@ public class Homework8 {
     }
 }
 
+class Test08 {
+    public static void main(String[] args) {
+        Father f = new Son();
+        System.out.println(f.x);
+    }
+}
+
+class Father {
+    int x = 10;
+
+    public Father() {
+        this.print();
+        x = 20;
+    }
+
+    public void print() {
+        System.out.println("Father.x = " + x);
+    }
+}
+
+class Son extends Father {
+    int x = 30;
+
+    public Son() {
+        this.print();
+        x = 40;
+    }
+
+    public void print() {
+        System.out.println("Son.x = " + x);
+    }
+}
