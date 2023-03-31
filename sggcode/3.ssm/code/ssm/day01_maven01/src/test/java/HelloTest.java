ed685df2c322cdcddcb61574e03e7f64811adf15
yf <15133695630@163.com>
2023-03-31 10:47:52 +0800

    第三阶段ssm,第一天学习maven

---
+++ sggcode/3.ssm/code/ssm/day01_maven01/src/test/java/HelloTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+/**
+ * Description:
+ *
+ * @author: yf
+ * @Create: 2023/03/31 -10:36
+ * @Version: 1.0
+ */
+public class HelloTest {
+    @Test
+    public void test1() {
+        Hello hello = new Hello();
+        hello.show("maven");
+    }
+}
