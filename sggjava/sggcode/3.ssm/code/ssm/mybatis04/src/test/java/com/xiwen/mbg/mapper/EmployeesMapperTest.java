62adc436f74e6efa72ff3223729e67b5ad2315d3
yf <15133695630@163.com>
2023-04-06 15:22:16 +0800

    分页插件

--- sggjava/sggcode/3.ssm/code/ssm/mybatis04/src/test/java/com/xiwen/mbg/mapper/EmployeesMapperTest.java
+++ sggjava/sggcode/3.ssm/code/ssm/mybatis04/src/test/java/com/xiwen/mbg/mapper/EmployeesMapperTest.java
@@ -1,5 +1,10 @@
 package com.xiwen.mbg.mapper;
 
+//import com.xiwen.mbg.bean.Employees;
+//import com.xiwen.mbg.bean.EmployeesExample;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.xiwen.mbg.bean.Employees;
 import com.xiwen.mbg.bean.EmployeesExample;
 import org.apache.ibatis.io.Resources;
@@ -58,6 +63,64 @@ public class EmployeesMapperTest {
 
         List<Employees> employees = employeesMapper.selectByExample(employeesExample);
         employees.stream().forEach(System.out::println);
+
+    }
+
+
+    @Test
+    public void selectByExample02() {
+        EmployeesMapper employeesMapper = sqlSession.getMapper(EmployeesMapper.class);
+
+        EmployeesExample employeesExample = new EmployeesExample();
+        EmployeesExample.Criteria employeesCriteria = employeesExample.createCriteria();
+        //模糊查询带h的
+        employeesCriteria.andLastNameLike("%h%");
+        //并且大于
+        employeesCriteria.andSalaryGreaterThan(4000d);
+
+        EmployeesExample.Criteria employeesCriteriaOr = new EmployeesExample().createCriteria();
+        employeesCriteriaOr.andIdLessThan(10);
+
+        //或者小于id10
+        employeesExample.or(employeesCriteriaOr);
+
+        PageHelper.offsetPage(2, 3);
+        List<Employees> employees = employeesMapper.selectByExample(employeesExample);
+
+        //设置分页信息
+        PageInfo<Employees> pageInfo = new PageInfo<>(employees, 3);
+
+        int pageNum = pageInfo.getPageNum(); // 当前页
+        System.out.println("pageNum = " + pageNum);
+        int pageSize = pageInfo.getPageSize();
+        System.out.println("pageSize = " + pageSize); // 当前页条数据
+        int pages = pageInfo.getPages();
+        System.out.println("pages = " + pages); //总页数
+        long total = pageInfo.getTotal();
+        System.out.println("total = " + total); //总条数
+        int startRow = pageInfo.getStartRow();
+        System.out.println("startRow = " + startRow);
+        int endRow = pageInfo.getEndRow();
+        System.out.println("endRow = " + endRow);
+
+        int prePage = pageInfo.getPrePage();
+        System.out.println("prePage = " + prePage); //上一页
+        int nextPage = pageInfo.getNextPage();
+        System.out.println("nextPage = " + nextPage); //下一页
+        int navigatePages = pageInfo.getNavigatePages(); //获取总页码
+        System.out.println("navigatePages = " + navigatePages);
+
+        boolean isFirstPage = pageInfo.isIsFirstPage(); //是否是第一页
+        System.out.println("isFirstPage = " + isFirstPage);
+        boolean isLastPage = pageInfo.isIsLastPage(); //是否是最后一页
+        System.out.println("isLastPage = " + isLastPage);
+        boolean hasPreviousPage = pageInfo.isHasPreviousPage(); //是否有上一页
+        System.out.println("hasPreviousPage = " + hasPreviousPage);
+        boolean hasNextPage = pageInfo.isHasNextPage(); //是否有下一页
+        System.out.println("hasNextPage = " + hasNextPage);
+
+
+        pageInfo.getList().stream().forEach(System.out::println);
 
     }
 }
