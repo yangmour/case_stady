02bcf2998d02fcd90c7ff48b840d906a51aad389
yf <15133695630@163.com>
2023-04-06 16:23:02 +0800

    主方法中使用逆向生成

---
+++ sggjava/sggcode/3.ssm/code/ssm/mybatis04/src/main/java/com/xiwen/mbg/MBG.java
@@ -0,0 +1,32 @@
+package com.xiwen.mbg;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Description:
+ *
+ * @author: yf
+ * @Create: 2023/04/06 -14:13
+ * @Version: 1.0
+ */
+public class MBG {
+    // 当前的父项目不行因为是普通java项目，不是maven项目所有不行，那就直接使用测试类的生成逆向生成文件
+    // 在maven项目中试过了可以
+    public static void main(String[] args) throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("mybatis04/src/main/resources/mbg.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
