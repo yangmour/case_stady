4220b78919d9ef3c9fa1b999230c37382eee879c
yf <15133695630@163.com>
2023-04-23 23:29:22 +0800

    补充

--- sggjava/sggcode/4.linux_redis_springboot/springboot/springboot2_demo/springboot_demo01/src/main/java/com/xiwen/boot/SpringBootDemo01Application.java
+++ sggjava/sggcode/4.linux_redis_springboot/springboot/springboot2_demo/springboot_demo01/src/main/java/com/xiwen/boot/SpringBootDemo01Application.java
@@ -13,6 +13,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 手动创建springboot
+ *
+ *      * 创建SpringBoot项目四步：
+ *      *   1.创建maven工程，引入父工程,规定了jar包版本信息
+ *      *   2.引入starter-web依赖，spring、springmvc、json、tomcat、logback
+ *      *   3.编写主启动类:@SpringBootApplication、main方法
+ *      *   4.编写业务逻辑类：一定要在主启动类所在包或者主启动类所在包的子包下
+ *
  */
 @SpringBootApplication
 public class SpringBootDemo01Application {
