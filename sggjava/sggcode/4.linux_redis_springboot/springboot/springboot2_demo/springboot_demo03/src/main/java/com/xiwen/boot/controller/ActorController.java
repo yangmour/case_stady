37acd3438c4bd4277e4730764a86879968378019
yf <15133695630@163.com>
2023-04-23 23:07:29 +0800

    启动应用测试

---
+++ sggjava/sggcode/4.linux_redis_springboot/springboot/springboot2_demo/springboot_demo03/src/main/java/com/xiwen/boot/controller/ActorController.java
@@ -0,0 +1,25 @@
+package com.xiwen.boot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Description:
+ *
+ * @author: yf
+ * @Create: 2023/04/23 -22:56
+ * @Version: 1.0
+ */
+
+@RestController
+@RequestMapping("/actor") //路径名最好和类名一样
+public class ActorController {
+
+    @GetMapping("hello")
+    public String hello(){
+
+        System.out.println("访问了hello");
+        return "基尼太美";
+    }
+}
