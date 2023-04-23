705ea0bf4bcc1bdac961b8e8a87b7ab019c683a6
yf <15133695630@163.com>
2023-04-23 17:18:35 +0800

    jedis连接集群

--- sggjava/sggcode/4.linux_redis_springboot/redis/jedis/src/main/java/com/xiwen/JedisTest.java
+++ sggjava/sggcode/4.linux_redis_springboot/redis/jedis/src/main/java/com/xiwen/JedisTest.java
@@ -1,7 +1,9 @@
 package com.xiwen;
 
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Description:
@@ -20,7 +22,7 @@ public class JedisTest {
         jedis.auth("xiwen123");
 //        System.out.println(jedis);
         //开始操作  方法名和redis的命令一样
-        jedis.set("k1","v1");
+        jedis.set("k1", "v1");
         String s = jedis.get("k1");
         System.out.println(s);
         //关闭连接
@@ -28,17 +30,36 @@ public class JedisTest {
 
     }
 
-    public static void main(String[] args) {
+    public static void main3(String[] args) {
         JedisPool jedisPool = new JedisPool("192.168.232.201", 6379);
         Jedis jedis = jedisPool.getResource();
         //设置密码
         jedis.auth("xiwen123");
 //        System.out.println(jedis);
         //开始操作  方法名和redis的命令一样
-        jedis.set("k2","v2");
+        jedis.set("k2", "v2");
         String s = jedis.get("k2");
         System.out.println(s);
         //关闭连接
         jedis.close();
+    }
+
+    public static void main(String[] args) {
+
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(100);
+        poolConfig.setMaxIdle(10);
+        poolConfig.setMinIdle(1);
+
+        Set<HostAndPort> nodes = new HashSet<>();
+        nodes.add(new HostAndPort("192.168.232.201", 7291));
+
+        String password = "redis123";
+        JedisCluster jedisCluster = new JedisCluster(nodes, 5000, 5000, 3, password, poolConfig);
+
+        jedisCluster.set("username","admin");
+        String username = jedisCluster.get("username");
+        System.out.println(username);
+        jedisCluster.close();
     }
 }
