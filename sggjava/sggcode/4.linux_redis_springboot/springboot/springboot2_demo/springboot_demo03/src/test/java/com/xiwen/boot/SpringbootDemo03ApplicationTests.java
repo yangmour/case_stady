4c0564eb1b55869a76d0effa2a286a95c0a94dc5
yf <15133695630@163.com>
2023-04-23 22:51:45 +0800

    选择阿里云网址配置脚手架初始化

---
+++ sggjava/sggcode/4.linux_redis_springboot/springboot/springboot2_demo/springboot_demo03/src/test/java/com/xiwen/boot/SpringbootDemo03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiwen.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemo03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
