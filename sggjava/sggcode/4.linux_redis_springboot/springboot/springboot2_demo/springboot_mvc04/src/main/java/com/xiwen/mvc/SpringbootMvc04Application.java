65bdc0027950f28fa9534f0df38e2f3c1fe352f3
yf <15133695630@163.com>
2023-04-25 18:26:21 +0800

    mvc整合

---
+++ sggjava/sggcode/4.linux_redis_springboot/springboot/springboot2_demo/springboot_mvc04/src/main/java/com/xiwen/mvc/SpringbootMvc04Application.java
@@ -0,0 +1,13 @@
+package com.xiwen.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMvc04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMvc04Application.class, args);
+	}
+
+}
