477ce6871cb7495abc6c70ac86553c71dcde4fa8
yf <15133695630@163.com>
2023-04-25 18:55:58 +0800

    自定义拦截器

---
+++ sggjava/sggcode/4.linux_redis_springboot/springboot/springboot2_demo/springboot_mvc04/src/main/java/com/xiwen/mvc/config/MvcConfig.java
@@ -0,0 +1,24 @@
+package com.xiwen.mvc.config;
+
+import com.xiwen.mvc.interceptor.MyInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Description:
+ *
+ * @author: yf
+ * @Create: 2023/04/25 -18:53
+ * @Version: 1.0
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Autowired
+    private MyInterceptor myInterceptor;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(myInterceptor).addPathPatterns("/**");
+    }
+}
---
+++ sggjava/sggcode/4.linux_redis_springboot/springboot/springboot2_demo/springboot_mvc04/src/main/java/com/xiwen/mvc/interceptor/MyInterceptor.java
@@ -0,0 +1,34 @@
+package com.xiwen.mvc.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Description:
+ *
+ * @author: yf
+ * @Create: 2023/04/25 -18:50
+ * @Version: 1.0
+ */
+@Component
+public class MyInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("访问了MyInterceptor.preHandle方法");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("访问了MyInterceptor.postHandle方法");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("访问了MyInterceptor.afterCompletion方法");
+    }
+}
