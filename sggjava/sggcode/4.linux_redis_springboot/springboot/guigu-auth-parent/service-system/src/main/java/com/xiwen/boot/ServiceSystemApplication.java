ded09b09a1f0ebaae53a65bfd546f32dbd5a8127
yf <15133695630@163.com>
2023-04-26 16:04:31 +0800

    引入依赖导入sql文件

---
+++ sggjava/sggcode/4.linux_redis_springboot/springboot/guigu-auth-parent/service-system/src/main/java/com/xiwen/boot/ServiceSystemApplication.java
@@ -0,0 +1,18 @@
+package com.xiwen.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description:
+ *
+ * @author: yf
+ * @Create: 2023/04/26 -16:02
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class ServiceSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceSystemApplication.class, args);
+    }
+}
---
+++ sggjava/sggcode/4.linux_redis_springboot/springboot/guigu-auth-parent/sql/表结构.sql
@@ -0,0 +1,132 @@
+﻿# Host: localhost  (Version 5.7.19-log)
+# Date: 2022-06-13 10:03:27
+# Generator: MySQL-Front 6.1  (Build 1.26)
+
+CREATE DATABASE `guigu-auth` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+use `guigu-auth`;
+
+#
+# Structure for table "sys_dept"
+#
+
+CREATE TABLE `sys_dept` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL DEFAULT '' COMMENT '部门名称',
+  `parent_id` bigint(20) DEFAULT '0' COMMENT '上级部门id',
+  `tree_path` varchar(255) DEFAULT ',' COMMENT '树结构',
+  `sort_value` int(11) DEFAULT '1' COMMENT '排序',
+  `leader` varchar(20) DEFAULT NULL COMMENT '负责人',
+  `phone` varchar(11) DEFAULT NULL COMMENT '电话',
+  `status` tinyint(1) DEFAULT '1' COMMENT '状态（1正常 0停用）',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `update_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `is_deleted` tinyint(3) NOT NULL DEFAULT '0' COMMENT '删除标记（0:不可用 1:可用）',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=2018 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT COMMENT='组织机构';
+
+#
+# Structure for table "sys_menu"
+#
+
+CREATE TABLE `sys_menu` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `parent_id` bigint(20) NOT NULL DEFAULT '0' COMMENT '所属上级',
+  `name` varchar(20) NOT NULL DEFAULT '' COMMENT '名称',
+  `type` tinyint(3) NOT NULL DEFAULT '0' COMMENT '类型(0:目录,1:菜单,2:按钮)',
+  `path` varchar(100) DEFAULT NULL COMMENT '路由地址',
+  `component` varchar(100) DEFAULT NULL COMMENT '组件路径',
+  `perms` varchar(100) DEFAULT NULL COMMENT '权限标识',
+  `icon` varchar(100) DEFAULT NULL COMMENT '图标',
+  `sort_value` int(11) DEFAULT NULL COMMENT '排序',
+  `status` tinyint(4) DEFAULT NULL COMMENT '状态(0:禁止,1:正常)',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `is_deleted` tinyint(3) NOT NULL DEFAULT '0' COMMENT '删除标记（0:不可用 1:可用）',
+  PRIMARY KEY (`id`),
+  KEY `idx_parent_id` (`parent_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COMMENT='菜单表';
+
+#
+# Structure for table "sys_post"
+#
+
+CREATE TABLE `sys_post` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '岗位ID',
+  `post_code` varchar(64) NOT NULL COMMENT '岗位编码',
+  `name` varchar(50) NOT NULL DEFAULT '' COMMENT '岗位名称',
+  `description` varchar(255) NOT NULL DEFAULT '' COMMENT '描述',
+  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '状态（1正常 0停用）',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `update_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `is_deleted` tinyint(3) NOT NULL DEFAULT '0' COMMENT '删除标记（0:不可用 1:可用）',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COMMENT='岗位信息表';
+
+#
+# Structure for table "sys_role"
+#
+
+CREATE TABLE `sys_role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '角色id',
+  `role_name` varchar(20) NOT NULL DEFAULT '' COMMENT '角色名称',
+  `role_code` varchar(20) DEFAULT NULL COMMENT '角色编码',
+  `description` varchar(255) DEFAULT NULL COMMENT '描述',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `is_deleted` tinyint(3) NOT NULL DEFAULT '0' COMMENT '删除标记（0:不可用 1:可用）',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COMMENT='角色';
+
+#
+# Structure for table "sys_role_menu"
+#
+
+CREATE TABLE `sys_role_menu` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `role_id` bigint(20) NOT NULL DEFAULT '0',
+  `menu_id` bigint(11) NOT NULL DEFAULT '0',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `is_deleted` tinyint(3) NOT NULL DEFAULT '0' COMMENT '删除标记（0:不可用 1:可用）',
+  PRIMARY KEY (`id`),
+  KEY `idx_role_id` (`role_id`),
+  KEY `idx_menu_id` (`menu_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8 COMMENT='角色菜单';
+
+#
+# Structure for table "sys_user"
+#
+
+CREATE TABLE `sys_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '会员id',
+  `username` varchar(20) NOT NULL DEFAULT '' COMMENT '用户名',
+  `password` varchar(32) NOT NULL DEFAULT '' COMMENT '密码',
+  `name` varchar(50) DEFAULT NULL COMMENT '姓名',
+  `phone` varchar(11) DEFAULT NULL COMMENT '手机',
+  `head_url` varchar(200) DEFAULT NULL COMMENT '头像地址',
+  `dept_id` bigint(20) DEFAULT NULL COMMENT '部门id',
+  `post_id` bigint(20) DEFAULT NULL COMMENT '岗位id',
+  `description` varchar(255) DEFAULT NULL COMMENT '描述',
+  `status` tinyint(3) DEFAULT NULL COMMENT '状态（1：正常 0：停用）',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `is_deleted` tinyint(3) NOT NULL DEFAULT '0' COMMENT '删除标记（0:不可用 1:可用）',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COMMENT='用户表';
+
+#
+# Structure for table "sys_user_role"
+#
+
+CREATE TABLE `sys_user_role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `role_id` bigint(20) NOT NULL DEFAULT '0' COMMENT '角色id',
+  `user_id` bigint(20) NOT NULL DEFAULT '0' COMMENT '用户id',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `is_deleted` tinyint(3) NOT NULL DEFAULT '0' COMMENT '删除标记（0:不可用 1:可用）',
+  PRIMARY KEY (`id`),
+  KEY `idx_role_id` (`role_id`),
+  KEY `idx_admin_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COMMENT='用户角色';
