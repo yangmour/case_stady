2a19ca2cc8615f276033e78c8d87aab0dadb2fe6
yf <15133695630@163.com>
2023-04-26 14:55:04 +0800

    添加序列化属性

--- sggjava/sggcode/4.linux_redis_springboot/springboot/springboot2_demo/springboot_case_demo04/src/main/java/com/xiwen/boot/bean/User.java
+++ sggjava/sggcode/4.linux_redis_springboot/springboot/springboot2_demo/springboot_case_demo04/src/main/java/com/xiwen/boot/bean/User.java
@@ -15,6 +15,8 @@ import java.io.Serializable;
 @AllArgsConstructor
 @Table(name = "tb_user")
 public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     @Id
     private Integer id;
     private String name;
