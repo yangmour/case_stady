381ff4ea1d7a1d56e765135c795ac439eabb745a
yf <15133695630@163.com>
2023-04-27 11:32:18 +0800

    整合swagger2测试接口，地址是ip:端口号/swagger_ui.html访问即可使用

--- sggjava/sggcode/4.linux_redis_springboot/springboot/ggkt_parent/service/server_vod/src/main/java/com/xiwen/ggkt/vod/controller/TeacherController.java
+++ sggjava/sggcode/4.linux_redis_springboot/springboot/ggkt_parent/service/server_vod/src/main/java/com/xiwen/ggkt/vod/controller/TeacherController.java
@@ -3,10 +3,11 @@ package com.xiwen.ggkt.vod.controller;
 
 import com.xiwen.ggkt.model.vod.Teacher;
 import com.xiwen.ggkt.vod.service.TeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,17 +19,27 @@ import java.util.List;
  * @author xw
  * @since 2023-04-26
  */
+@Api(tags = "教师管理接口")
 @RestController
 @RequestMapping("/vod/teacher")
 public class TeacherController {
     @Autowired
     private TeacherService teacherService;
 
+    @ApiOperation("查询所有教师")
     @GetMapping("findAll")
     public List<Teacher> findAll() {
         List<Teacher> list = teacherService.list();
         System.out.println(list);
         return list;
+    }
+
+    @ApiOperation("根据id逻辑删除教师")
+    @DeleteMapping("/{id}")
+    public boolean removeById(
+            @ApiParam(name = "id", value = "ID") @PathVariable Integer id) {
+        return teacherService.removeById(id);
+
     }
 }
 
