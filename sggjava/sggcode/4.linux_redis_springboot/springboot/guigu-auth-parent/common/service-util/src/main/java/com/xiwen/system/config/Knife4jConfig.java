24a45450de985ac1daaeef403cb187a1b125408d
yf <15133695630@163.com>
2023-04-28 09:20:01 +0800

    添加token

--- sggjava/sggcode/4.linux_redis_springboot/springboot/guigu-auth-parent/common/service-util/src/main/java/com/xiwen/system/config/Knife4jConfig.java
+++ sggjava/sggcode/4.linux_redis_springboot/springboot/guigu-auth-parent/common/service-util/src/main/java/com/xiwen/system/config/Knife4jConfig.java
@@ -3,13 +3,19 @@ package com.xiwen.system.config;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Description:
@@ -24,6 +30,19 @@ public class Knife4jConfig {
 
     @Bean
     public Docket defaultApi2() {
+
+        List<Parameter> pars = new ArrayList<>();
+        ParameterBuilder tokenPar = new ParameterBuilder();
+
+        tokenPar.name("token")
+                .description("用户token")
+                .defaultValue("")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(false)
+                .build();
+        pars.add(tokenPar.build());
+
         Docket docket = new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 //分组名称
@@ -33,7 +52,8 @@ public class Knife4jConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.xiwen"))
                 //只显示admin路径下的页面
                 .paths(PathSelectors.regex("/admin/.*"))
-                .build();
+                .build()
+                .globalOperationParameters(pars);
         return docket;
     }
 
