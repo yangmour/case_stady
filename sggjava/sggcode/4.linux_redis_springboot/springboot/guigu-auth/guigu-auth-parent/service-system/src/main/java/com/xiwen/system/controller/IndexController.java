22dc29280b606cb5a6345ba0da164dd689a29220
yf <15133695630@163.com>
2023-04-28 23:03:35 +0800

    后端提供接口，前端设置请求路径，代理方式等

---
+++ sggjava/sggcode/4.linux_redis_springboot/springboot/guigu-auth/guigu-auth-parent/service-system/src/main/java/com/xiwen/system/controller/IndexController.java
@@ -0,0 +1,45 @@
+package com.xiwen.system.controller;
+
+import com.xiwen.common.result.Result;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Description:
+ *
+ * @author: yf
+ * @Create: 2023/04/28 -22:33
+ * @Version: 1.0
+ */
+@RestController
+@RequestMapping("/admin/system/index")
+public class IndexController {
+
+    @PostMapping("/login")
+    public Result<Object> login(@RequestBody Map<String, Object> map) {
+        map.put("token", "admin");
+        return Result.ok(map);
+    }
+
+    @GetMapping("/info")
+    public Result<Object> info(String token) {
+        HashMap<String, Object> map = null;
+        if ("admin".equals(token)) {
+            map = new HashMap<>();
+            ArrayList<String> list = new ArrayList<>();
+            list.add("admin");
+            map.put("roles", list);
+            map.put("introduction", "返回信息");
+            map.put("name", "admin");
+            map.put("avatar", "https://inews.gtimg.com/newsapp_bt/0/13294928426/1000");
+        } else {
+            return Result.fail();
+        }
+        return Result.ok(map);
+    }
+
+
+}
