8250ebcf534ac09e3aace7cadf3c5287489658e1
yf <15133695630@163.com>
2023-05-16 10:03:03 +0800

    能者多劳

--- sggjava/sggcode/4.linux_redis_springboot/rabbitmq/rabbitmq/spring-rabbitmq-consumer/src/main/java/com/xiwen/mq/listener/SpringWorkQueueListener02.java
+++ sggjava/sggcode/4.linux_redis_springboot/rabbitmq/rabbitmq/spring-rabbitmq-consumer/src/main/java/com/xiwen/mq/listener/SpringWorkQueueListener02.java
@@ -1,7 +1,8 @@
 package com.xiwen.mq.listener;
 
+import com.rabbitmq.client.Channel;
 import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessageListener;
+import org.springframework.amqp.rabbit.listener.api.ChannelAwareMessageListener;
 
 /**
  * Description:
@@ -10,9 +11,10 @@ import org.springframework.amqp.core.MessageListener;
  * @Create: 2023/05/15 -18:30
  * @Version: 1.0
  */
-public class SpringWorkQueueListener02 implements MessageListener {
+public class SpringWorkQueueListener02 implements ChannelAwareMessageListener {
     @Override
-    public void onMessage(Message message) {
+    public void onMessage(Message message, Channel channel) throws Exception {
         System.out.println("消费者2:" + new String(message.getBody()));
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
     }
 }
