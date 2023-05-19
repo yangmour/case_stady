64cf29216da5170da634d73d526f05f55b605cbe
yf <15133695630@163.com>
2023-05-19 15:03:26 +0800

    spring-cloud中hystrix容错框架feign+hystrix 可视化

--- sggjava/sggcode/4.linux_redis_springboot/spring-cloud/cloud-eureka-client-consumer-feign80/src/main/java/com/xiwen/cloud/CloudEurekaClientConsumerFeign80Application.java
+++ sggjava/sggcode/4.linux_redis_springboot/spring-cloud/cloud-eureka-client-consumer-feign80/src/main/java/com/xiwen/cloud/CloudEurekaClientConsumerFeign80Application.java
@@ -5,10 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
+@EnableHystrixDashboard
 @EnableCircuitBreaker // 开启断路保护功能
 @MapperScan("com.xiwen.cloud.mapper")
 @EnableFeignClients //开启fegin客户端
