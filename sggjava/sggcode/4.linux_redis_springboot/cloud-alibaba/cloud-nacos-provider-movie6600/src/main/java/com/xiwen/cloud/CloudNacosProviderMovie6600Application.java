6b5375ff44b0d9d3797a4572298ad98b52a6b423
yf <15133695630@163.com>
2023-05-20 10:00:41 +0800

    nacos初始化

---
+++ sggjava/sggcode/4.linux_redis_springboot/cloud-alibaba/cloud-nacos-provider-movie6600/src/main/java/com/xiwen/cloud/CloudNacosProviderMovie6600Application.java
@@ -0,0 +1,13 @@
+package com.xiwen.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNacosProviderMovie6600Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudNacosProviderMovie6600Application.class, args);
+    }
+
+}
---
+++ sggjava/sggcode/4.linux_redis_springboot/cloud-alibaba/cloud-nacos-provider-movie6600/src/test/java/com/xiwen/cloud/CloudNacosProviderMovie6600ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiwen.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudNacosProviderMovie6600ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
