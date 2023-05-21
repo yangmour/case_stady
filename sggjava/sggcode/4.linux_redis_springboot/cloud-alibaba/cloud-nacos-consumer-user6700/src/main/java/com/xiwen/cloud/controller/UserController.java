d11b3838b5ad653cb82c5016dd5b926e3accf01d
yf <15133695630@163.com>
2023-05-21 17:37:12 +0800

    nacos的配置中心测试

--- sggjava/sggcode/4.linux_redis_springboot/cloud-alibaba/cloud-nacos-consumer-user6700/src/main/java/com/xiwen/cloud/controller/UserController.java
+++ sggjava/sggcode/4.linux_redis_springboot/cloud-alibaba/cloud-nacos-consumer-user6700/src/main/java/com/xiwen/cloud/controller/UserController.java
@@ -3,6 +3,8 @@ package com.xiwen.cloud.controller;
 import com.xiwen.cloud.bean.User;
 import com.xiwen.cloud.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +19,27 @@ import java.util.Map;
  * @Create: 2023/05/20 -11:46
  * @Version: 1.0
  */
+@RefreshScope // 配置中心更新配置注解
 @RestController
 @RequestMapping("user")
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Value("${server.port}")
+    private Integer port;
+    @Value("${jdbc.username}")
+    private String username;
+
+    @Value("${datasource.username}")
+    private String datasourceUsername;
+    @Value("${datasource.password}")
+    private String datasourcePassword;
+    @Value("${redis.username}")
+    private String redisUsername;
+    @Value("${cs.aaa}")
+    private String aaa;
 
     @GetMapping("info/{id}")
     public User info(@PathVariable Integer id) {
@@ -31,6 +48,9 @@ public class UserController {
 
     @GetMapping("movie/{id}")
     public Map<String, Object> movieAndUser(@PathVariable Integer id) {
+        System.out.println(port + ":" + username);
+        System.out.println("----------------");
+        System.out.println(datasourceUsername + ":" + datasourcePassword + ";" + redisUsername + ";" + aaa);
         return userService.movieAndUser(id);
     }
 
