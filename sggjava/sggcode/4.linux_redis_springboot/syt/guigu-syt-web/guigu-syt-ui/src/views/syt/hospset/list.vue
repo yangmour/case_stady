51114dc8497c16cb2f9989b3eabed9f13ad54e13
yf <15133695630@163.com>
2023-05-30 20:01:31 +0800

    操作按钮和搜索框、清空搜索框

--- sggjava/sggcode/4.linux_redis_springboot/syt/guigu-syt-web/guigu-syt-ui/src/views/syt/hospset/list.vue
+++ sggjava/sggcode/4.linux_redis_springboot/syt/guigu-syt-web/guigu-syt-ui/src/views/syt/hospset/list.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="app-container">
-    <!-- 表单 -->
+    <!-- 搜索表单 -->
+    <!--查询表单-->
+    <el-form :inline="true" class="demo-form-inline">
+        <el-form-item>
+            <el-input v-model="searchObj.hosname" placeholder="医院名称"/>
+        </el-form-item>
+        <el-form-item>
+            <el-input v-model="searchObj.hoscode" placeholder="医院编号"/>
+        </el-form-item>
+        <el-button type="primary" icon="el-icon-search" @click="searchData()" :loading="listLoading">查询</el-button>
+        <el-button type="default" @click="resetData()">清空</el-button>
+    </el-form>
+    <!-- 表格 -->
     <el-table
       :data="tableData"
       style="width: 100%"
@@ -32,10 +44,36 @@
           {{ scope.row.status == 1 ? "可用" : "禁用" }}
         </template>
       </el-table-column>
+
+      <el-table-column label="操作" align="center">
+        <template slot-scope="scope">
+          <el-button
+            type="primary"
+            size="mini"
+            @click="handleEdit(scope.$index, scope.row)"
+            class="el-icon-edit"
+            >编辑</el-button
+          >
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)"
+            class="el-icon-delete"
+            >删除</el-button
+          >
+          <el-button
+            size="mini"
+            type="warning"
+            @click="handleDelete(scope.$index, scope.row)"
+            class="el-icon-lock"
+            >锁定</el-button
+          >
+        </template>
+      </el-table-column>
     </el-table>
     <!-- 分页 -->
     <el-pagination
-      style="padding: 10px;text-align: center;"
+      style="padding: 10px; text-align: center"
       @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
       :current-page="pageNum"
@@ -72,14 +110,21 @@ export default {
     };
   },
   methods: {
-    handleSizeChange(val) {
-        this.pageSize = val
+    searchData(){
         this.fetchData()
     },
-    handleCurrentChange(val) {
-        this.fetchData(val)
+    resetData(){
+        this.searchObj = {}
+        this.fetchData();
     },
-    fetchData(pageNum = 1,pageSize, searchObj) {
+    handleSizeChange(val) {
+      this.pageSize = val;
+      this.fetchData();
+    },
+    handleCurrentChange(val) {
+      this.fetchData(val);
+    },
+    fetchData(pageNum = 1, pageSize, searchObj) {
       this.pageNum = pageNum;
       hospsetApi
         .getPageList(this.pageNum, this.pageSize, this.searchObj)
