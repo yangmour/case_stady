9f5d0390fd125f943136cd0323ca965445f14fcb
yf <15133695630@163.com>
2023-07-02 14:44:24 +0800

    自定义线程通信

--- sggjava/sggcode/5.mysql-juc-jvm-store/juc-demo/src/main/java/com/xiwen/juc/day02/NotifyWaitDemo.java
+++ sggjava/sggcode/5.mysql-juc-jvm-store/juc-demo/src/main/java/com/xiwen/juc/day02/NotifyWaitDemo.java
@@ -1,5 +1,9 @@
 package com.xiwen.juc.day02;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Description:
  *
@@ -45,11 +49,58 @@ class ShareDataOne {
 
 }
 
+class ShareDataOne2 {
+    private Integer number = 0;
+    // 锁
+    private final Lock lock = new ReentrantLock();
+    // 配一把钥匙
+    private Condition condition = lock.newCondition();
+
+    /**
+     * 增1
+     */
+    public void increment() throws InterruptedException {
+        lock.lock();
+        // 等于1 开始堵塞
+        while (number != 0) {
+            condition.await();
+        }
+        // 开始生产
+        number++;
+        System.out.println(Thread.currentThread().getName() + "生产:" + number);
+
+        // 通知其他线程
+        condition.signalAll();
+        lock.unlock();
+    }
+
+    /**
+     * 减1
+     */
+    public void decrement() throws InterruptedException {
+        lock.lock();
+        // 等于0 开始堵塞
+        while (number != 1) {
+            condition.await();
+        }
+        // 开始消费
+        number--;
+        System.out.println(Thread.currentThread().getName() + "消费:" + number);
+
+        //通知其他线程
+        condition.signalAll();
+        lock.unlock();
+
+    }
+
+}
+
 public class NotifyWaitDemo {
 
     public static void main(String[] args) {
         //	两个线程操作一个初始值为0的变量，实现一个线程对变量增加1，一个线程对变量减少1，交替10轮。
-        ShareDataOne shareDataOne = new ShareDataOne();
+//        ShareDataOne shareDataOne = new ShareDataOne();
+        ShareDataOne2 shareDataOne = new ShareDataOne2();
 
         int size = 10;
 
---
+++ sggjava/sggcode/5.mysql-juc-jvm-store/juc-demo/src/main/java/com/xiwen/juc/day02/ThreadOrderAccess.java
@@ -0,0 +1,117 @@
+package com.xiwen.juc.day02;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Description:
+ *
+ * @author: yf
+ * @Create: 2023/07/02 -14:15
+ * @Version: 1.0
+ */
+
+class ShareDataTwo {
+    private final Lock lock = new ReentrantLock();
+    private Integer flag = 1; // 1:打印五次，2:打印10次，3:打印15
+    // 配钥匙
+    private final Condition condition1 = lock.newCondition();
+    private final Condition condition2 = lock.newCondition();
+    private final Condition condition3 = lock.newCondition();
+
+    public void print5() {
+        lock.lock();
+        try {
+            while (flag != 1) {
+                condition1.await();
+            }
+            for (int i = 0; i < 5; i++) {
+                System.out.println(Thread.currentThread().getName() + "AA" + (i + 1));
+            }
+
+            flag = 2;
+            condition2.signal();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void print10() {
+        lock.lock();
+        try {
+            while (flag != 2) {
+                condition2.await();
+            }
+            for (int i = 0; i < 10; i++) {
+                System.out.println(Thread.currentThread().getName() + "BB" + (i + 1));
+            }
+
+            flag = 3;
+            condition3.signal();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void print15() {
+
+        lock.lock();
+        try {
+            while (flag != 3) {
+                condition3.await();
+            }
+            for (int i = 0; i < 15; i++) {
+                System.out.println(Thread.currentThread().getName() + "CC" + (i + 1));
+            }
+
+            flag = 1;
+            condition1.signal();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
+    }
+}
+
+public class ThreadOrderAccess {
+    public static void main(String[] args) {
+
+        /**
+         * 案例：
+         * 多线程之间按顺序调用，实现A->B->C。三个线程启动，要求如下：
+         * AA打印5次，BB打印10次，CC打印15次
+         * 接着
+         * AA打印5次，BB打印10次，CC打印15次
+         * 。。。打印
+         * 10轮
+         */
+
+        ShareDataTwo shareDataTwo = new ShareDataTwo();
+
+
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                shareDataTwo.print5();
+            }
+        }, "AA").start();
+
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                shareDataTwo.print10();
+            }
+        }, "BB").start();
+
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                shareDataTwo.print15();
+            }
+        }, "CC").start();
+
+    }
+}
